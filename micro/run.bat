a73b1caacdc2b46ab7d2953e4425e4c7f185c049
Ragasits Csaba <ragasits.csaba@gmail.com>
2020-06-19 21:37:51 +0200

    + Migrate to Eclipse microprofile 3.3, Paraya Micro 5.2020.2

--- micro/run.bat
+++ micro/run.bat
@@ -3,6 +3,8 @@ set KRAKEE_PROXYENABLED=false
 
 set KRAKEE_RUNCANDLE=false
 
-java -jar payara-micro-5.201.jar --logtofile .\log\krakee --noCluster --deploy krakEE-80.war --rootDir .\root
+REM java -jar payara-micro-5.201.jar --logtofile .\log\krakee --noCluster --deploy krakEE-80.war --rootDir .\root
+
+java -jar payara-micro-5.2020.2.jar --logtofile .\log\krakee --noCluster --deploy krakEE-90.war --rootDir .\root
 
 pause
