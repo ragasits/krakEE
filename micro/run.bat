0258671f8e4a640318f11cfce21e8beb77857a09
Ragasits Csaba <ragasits.csaba@gmail.com>
2021-01-10 16:47:19 +0100

    + Migrate to MicroProfile 4.0

--- micro/run.bat
+++ micro/run.bat
@@ -3,6 +3,6 @@ set KRAKEE_PROXYENABLED=false
 
 set KRAKEE_RUNCANDLE=true
 
-java -jar payara-micro-5.2020.4.jar --logtofile .\log\krakee --noCluster --deploy krakEE-90.war --rootDir .\root
+java -jar payara-micro-5.2020.7.jar --logtofile .\log\krakee --noCluster --deploy krakEE-110.war --rootDir .\root
 
 pause
