1396ffdfa59e7252246e2da4baa7f4e6d7eff127
Ragasits Csaba <ragasits.csaba@gmail.com>
2021-11-29 21:20:06 +0100

    + Migrate JDK8 to Eclipse Adoptium OpenJDK11U-jdk_x64_windows_hotspot_11.0.13_8 + Change deprecated source code

--- src/main/java/krakee/Common.java
+++ src/main/java/krakee/Common.java
@@ -7,6 +7,7 @@ package krakee;
 
 import java.math.BigDecimal;
 import static java.math.BigDecimal.ROUND_HALF_UP;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 /**
@@ -44,9 +45,9 @@ public class Common {
         BigDecimal x1 = new BigDecimal(Math.sqrt(A.doubleValue()));
         while (!x0.equals(x1)) {
             x0 = x1;
-            x1 = A.divide(x0, SCALE, ROUND_HALF_UP);
+            x1 = A.divide(x0, SCALE, RoundingMode.HALF_UP);
             x1 = x1.add(x0);
-            x1 = x1.divide(TWO, SCALE, ROUND_HALF_UP);
+            x1 = x1.divide(TWO, SCALE, RoundingMode.HALF_UP);
 
         }
         return x1;
